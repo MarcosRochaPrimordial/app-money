e2a01aa5de0ecaac8f5c4d2faffb3168c98bfa96
Marcos A. Rocha <m.vila.nova.neto@avanade.com>
2019-07-14 22:38:12 -0300

    Fonts to ngsw and self update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from './core/services/auth.service';
 import { ToggleState } from './core/interfaces/toggleState';
 import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material';
 import { AppDateAdapter, APP_DATE_FORMATS } from './shared/adapters/date.adapter';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -22,8 +23,13 @@ export class AppComponent implements OnInit {
   show: boolean = false;
 
   constructor(
-    private authService: AuthService
-  ) {}
+    private authService: AuthService,
+    private update: SwUpdate
+  ) {
+    this.update.available.subscribe(event => {
+      update.activateUpdate().then(() => document.location.reload());
+    });
+  }
 
   ngOnInit() {
     this.authService.loginState.subscribe((loginState: ToggleState) => {
