8cac0dbfe6edaead8f6d8bfa132fd12810f5aa6d
Marcos <markossdeath@gmail.com>
2021-06-02 00:00:50 -0300

    fixed spendings canvas

--- src/app/home/components/spendings/spendings.component.ts
+++ src/app/home/components/spendings/spendings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-spendings',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpendingsComponent implements OnInit {
 
+  @ViewChild('fixed_spendings', { static: true }) fixed_spendings!: ElementRef;
+  @ViewChild('outgoings', { static: true }) outgoings!: ElementRef;
+  @ViewChild('incomes', { static: true }) incomes!: ElementRef;
+  @ViewChild('fixed_spendings_button', { static: true }) fixed_spendings_button!: ElementRef;
+  @ViewChild('outgoings_button', { static: true }) outgoings_button!: ElementRef;
+  @ViewChild('incomes_button', { static: true }) incomes_button!: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.fixed_spendings.nativeElement.style.display = "block";
+    this.fixed_spendings_button.nativeElement.className += " active";
+  }
+
+  openTab(evt: any, tab: string) {
+    this.fixed_spendings.nativeElement.style.display = "none";
+    this.outgoings.nativeElement.style.display = "none";
+    this.incomes.nativeElement.style.display = "none";
+
+    this.fixed_spendings_button.nativeElement.className = this.fixed_spendings_button.nativeElement.className.replace(" active", "");
+    this.outgoings_button.nativeElement.className = this.outgoings_button.nativeElement.className.replace(" active", "");
+    this.incomes_button.nativeElement.className = this.incomes_button.nativeElement.className.replace(" active", "");
+
+    switch (tab) {
+      case 'fixed_spendings':
+        this.fixed_spendings.nativeElement.style.display = "block";
+        break;
+      case 'outgoings':
+        this.outgoings.nativeElement.style.display = "block";
+        break;
+      case 'incomes':
+        this.incomes.nativeElement.style.display = "block";
+        break;
+    }
+    evt.currentTarget.className += " active";
   }
 
 }
---
+++ src/app/shared/models/Spending.model.ts
@@ -0,0 +1,4 @@
+export interface Spendings {
+    description: string;
+    importance: number;
+}
